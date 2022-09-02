8afd82bb0f66503cd2f821ef25b1db60214253cd
MrCharouon <ali.khatami2009@gmail.com>
2022-09-02 03:13:47 -0400

    Create the address and open it in the browser

---
+++ torrents.py
@@ -0,0 +1,33 @@
+ #!/usr/bin/python
+import argparse
+import webbrowser
+import time
+parser = argparse.ArgumentParser(description="""Documentation program""", usage="""
+      For example:
+    \t python main.py -S "House of the Dragon S01E02" """, epilog="and that's how you'd add and remove stocks on your watchlist")
+parser.add_argument('-S', action='store', dest='your_input', help='your input in here')
+args = parser.parse_args()
+
+try:
+    stock_input_add = (args.your_input)
+    # print(f'\n{stock_input_add}\n')
+
+    input_args= stock_input_add.replace(" ", "+")
+    url1= (f"https://thepiratebay.org/search.php?q={input_args}")
+    url2= (f'https://rarbg.to/torrents.php?search={input_args}')
+
+    print(f"{url1}\n{url2}\n")
+
+    webbrowser.open_new(url1)
+    time.sleep(2)
+    webbrowser.open_new(url2)
+    time.sleep(2)
+    print('\ndone')
+
+except:
+    print("error")
+
+
+
+
+
